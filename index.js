d8201ad0f341608891d0fe46efb0598c03801481
rinadkarim <rinadjobs@gmail.com>
2024-01-31 16:30:47 -0500

    Array Filter Method Lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(DriversNamesArray, StringToMatch){
+    return DriversNamesArray.filter((DriverName) => {
+        return DriverName.toUpperCase() === StringToMatch.toUpperCase()    
+    });
+}
+
+function fuzzyMatch(ArrayOfDrivers, StringInQ){
+    return ArrayOfDrivers.filter((DriversName) => {
+        return DriversName[0] === StringInQ[0]
+    });
+}
+
+function matchName(ArrayOfDriverObjects, StringInQ){
+    return ArrayOfDriverObjects.filter((DriverObject) => {
+        return DriverObject.name === StringInQ
+    });
+}
